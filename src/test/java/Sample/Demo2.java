0fec440780a54b36a7163306a5b541e840d03dd1
Jhansi91 <jhansi2391@gmail.com>
2023-08-03 20:06:28 +0530

    Demo2 is commited

---
+++ src/test/java/Sample/Demo2.java
@@ -0,0 +1,13 @@
+package Sample;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+	@Test
+	public void demoTest()
+	{
+		System.out.println("this is my  Demo2 first commit");
+		System.out.println("this is my  Demo2 second commit");
+	}
+
+}
